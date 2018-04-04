c030042f5ecd948802cc3911aa9483b506591fc8
Guijie Lee <landriesnidis@yeah.net>
2018-04-04 23:51:11 +0800

    新增适配器功能接口

---
+++ Adapter/Function/IEccCmd.cs
@@ -0,0 +1,17 @@
+﻿using ECC_sdk_windows.EccArgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECC_sdk_windows.Adapter
+{
+    public interface IEccCmd
+    {
+        //发送API_KEY
+        void EccCmd_SendAPIKey(AsyncCallback callback, SendAPIKeyCmdArgs args);
+        //发送单个设备的控制命令
+        void EccCmd_ControlItem(AsyncCallback callback, ControlItemCmdArgs args);
+    }
+}
---
+++ Adapter/Function/IEccEvevt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECC_sdk_windows.Adapter
+{
+    public interface IEccEvevt
+    {
+        //接收服务端询问API_KEY
+
+        //接收更新设备项的数据集
+
+        //接收紧急消息
+
+        //
+    }
+}
