60d6db9e999fa5db45dd5d0fa3311e631b6e3bf1
Guijie Lee <landriesnidis@yeah.net>
2018-04-05 20:21:05 +0800

    将ActionJson划分为EventJson和CmdJson

--- Utils/ActionJson.cs
+++ Utils/ActionJson.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using ECC_sdk_windows.EccArgs;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
@@ -8,14 +9,29 @@ using System.Threading.Tasks;
 
 namespace ECC_sdk_windows
 {
+
     class ActionJson
+    {
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        public static EventJson Deserialize(string strJson)
+        {
+            return JsonConvert.DeserializeObject<EventJson>(strJson);
+        }
+    }
+
+
+    class EventJson : ActionJson
     {
         public string Action { set; get; }
         public Boolean Flag { set; get; }
         public string Content { set; get; }
         public string Message { set; get; }
 
-        public ActionJson(string strJson)
+        public EventJson(string strJson)
         {
             JObject json = JObject.Parse(strJson);
 
@@ -24,15 +40,12 @@ namespace ECC_sdk_windows
             Content = (string)json["content"];
             Message = (string)json["message"];
         }
+    }
 
-        public override string ToString()
-        {
-            return JsonConvert.SerializeObject(this);
-        }
-
-        public static ActionJson Deserialize(string strJson)
-        {
-           return JsonConvert.DeserializeObject<ActionJson>(strJson);
-        }
+    class CmdJson : ActionJson
+    {
+        public string Action { set; get; }
+        public string Content { set; get; }
     }
 }
+ 
