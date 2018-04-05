903d9787322a4f4523fa4418ee4e2fedd62b39af
Guijie Lee <landriesnidis@yeah.net>
2018-04-05 20:20:33 +0800

    新增必要的Exception

--- Exception/EccException.cs
+++ Exception/EccException.cs
@@ -6,8 +6,39 @@ using System.Threading.Tasks;
 
 namespace ECCIoT_sdk_windows
 {
-    class EccException
+    /// <summary>
+    /// 尚未对ECCIoT设置APIKey
+    /// </summary>
+    public class NotSetAPIKeyException : Exception
     {
-
+        public override string ToString()
+        {
+            return "No APIKey has been set for ECCIoT.";
+        }
     }
+
+    /// <summary>
+    /// 尚未完成与服务器之间的连接
+    /// </summary>
+    public class UnconnectedCompletionException : Exception
+    {
+        public override string ToString()
+        {
+            return "The connection to the server has not yet been completed.";
+        }
+    }
+
+    /// <summary>
+    /// 未连接服务器
+    /// </summary>
+    public class UnconnectedServerException : Exception
+    {
+        public override string ToString()
+        {
+            return "No connection to the server.";
+        }
+    }
+
+
+
 }
