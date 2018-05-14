89b8129547059cb682af93fa4fe5955bafefb224
Guijie Lee <landriesnidis@yeah.net>
2018-05-15 02:33:59 +0800

    新增用于保存参数类型对应的参数的Attribute

---
+++ Manager/Utils/ArgsAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECC_sdk_windows.Manager.Utils
+{
+    public class ArgsAttribute : Attribute
+    {
+        public string Action { get; set; }
+        public ArgsAttribute(String action)
+        {
+            Action = action;
+        }
+    }
+}
